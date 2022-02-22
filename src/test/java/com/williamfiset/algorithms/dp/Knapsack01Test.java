f03cb2de26c6337b14255fdfb187df6f13aad684
OscarIngels <oscar.ingels@gmail.com>
2022-02-22 17:40:24 +0100

    feat: Added testfile and test to test directory. For some reason last commit did not contain the actual test, but that should be fixed now. #issue #7

--- src/test/java/com/williamfiset/algorithms/dp/Knapsack01Test.java
+++ src/test/java/com/williamfiset/algorithms/dp/Knapsack01Test.java
@@ -1,2 +1,25 @@
-package com.williamfiset.algorithms.dp;public class Knapsack01Test {
+package com.williamfiset.algorithms.dp;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.primitives.Ints;
+import com.williamfiset.algorithms.utils.TestUtils;
+import java.util.*;
+import org.junit.rules.ExpectedException;
+import org.junit.*;
+public class Knapsack01Test {
+    @Test
+    public void knapsack01InvalidInputTest1(){
+        int capacity = 10;
+        int[] V = {1, 4, 8};
+        int[] W = {3, 3, 5, 6};
+        try {
+            int val = Knapsack_01.knapsack(capacity, W, V);
+
+        } catch (IllegalArgumentException e) {
+
+            assertThat(e).hasMessageThat().contains("Invalid input");
+        }
+
+    }
+
 }
