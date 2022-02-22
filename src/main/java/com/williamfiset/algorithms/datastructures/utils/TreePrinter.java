ff8dd2cf09b09da551b3f2aba5a9d0747b97136c
theanot <theanot@kth.se>
2022-02-22 12:56:23 +0100

    added coverag aray

--- src/main/java/com/williamfiset/algorithms/datastructures/utils/TreePrinter.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/utils/TreePrinter.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class TreePrinter {
 
+  static boolean test[];
+
   /** Node that can be printed */
   public interface PrintableNode {
 
@@ -20,7 +22,9 @@ public class TreePrinter {
   }
 
   // Print a binary tree.
+
   public static String getTreeDisplay(PrintableNode root) {
+    test = new boolean[13];
 
     StringBuilder sb = new StringBuilder();
     List<List<String>> lines = new ArrayList<List<String>>();
@@ -35,24 +39,27 @@ public class TreePrinter {
       nn = 0;
       List<String> line = new ArrayList<String>();
       for (PrintableNode n : level) {
+        
         if (n == null) {
+          test[0] = true;
           line.add(null);
           next.add(null);
           next.add(null);
         } else {
+          test[1] = true;
           String aa = n.getText();
           line.add(aa);
-          if (aa.length() > widest) widest = aa.length();
+          if (aa.length() > widest) {widest = aa.length(); test[2] = true;}
 
           next.add(n.getLeft());
           next.add(n.getRight());
 
-          if (n.getLeft() != null) nn++;
-          if (n.getRight() != null) nn++;
+          if (n.getLeft() != null) nn++; test[3] = true;
+          if (n.getRight() != null) nn++; test[4] = true;
         }
       }
 
-      if (widest % 2 == 1) widest++;
+      if (widest % 2 == 1) widest++ ; test[5] = true;
 
       lines.add(line);
 
@@ -67,14 +74,18 @@ public class TreePrinter {
       List<String> line = lines.get(i);
       int hpw = (int) Math.floor(perpiece / 2f) - 1;
       if (i > 0) {
+        test[6] = true;
         for (int j = 0; j < line.size(); j++) {
 
           // split node
           char c = ' ';
           if (j % 2 == 1) {
+            test[7] = true;
             if (line.get(j - 1) != null) {
+              test[8] = true;
               c = (line.get(j) != null) ? '#' : '#';
             } else {
+              test[9] = true;
               if (j < line.size() && line.get(j) != null) c = '#';
             }
           }
@@ -82,10 +93,12 @@ public class TreePrinter {
 
           // lines and spaces
           if (line.get(j) == null) {
+            test[10] = true;
             for (int k = 0; k < perpiece - 1; k++) {
               sb.append(' ');
             }
           } else {
+            test[11] = true;
             for (int k = 0; k < hpw; k++) {
               sb.append(j % 2 == 0 ? " " : "#");
             }
@@ -99,7 +112,7 @@ public class TreePrinter {
       }
       for (int j = 0; j < line.size(); j++) {
         String f = line.get(j);
-        if (f == null) f = "";
+        if (f == null) f = ""; test[12] = true;
         int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
         int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
 
@@ -115,6 +128,14 @@ public class TreePrinter {
 
       perpiece /= 2;
     }
+    printDebugInfo();
     return sb.toString();
+    
+  }
+  public static void printDebugInfo() {
+    for(int i = 0; i < test.length; i++) {
+      System.out.println(" " + test[i]);
+    }
+
   }
 }
