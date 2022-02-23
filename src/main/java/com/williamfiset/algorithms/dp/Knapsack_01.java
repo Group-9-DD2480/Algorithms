85223fafbc872d0222a8a6577f1e2c3d286d94d8
OscarIngels <oscar.ingels@gmail.com>
2022-02-23 22:40:47 +0100

    feat: Refactor knapsack method. Issue #22

--- src/main/java/com/williamfiset/algorithms/dp/Knapsack_01.java
+++ src/main/java/com/williamfiset/algorithms/dp/Knapsack_01.java
@@ -32,24 +32,7 @@ public class Knapsack_01 {
 
     // Initialize a table where individual rows represent items
     // and columns represent the weight of the knapsack
-    int[][] DP = new int[N + 1][capacity + 1];
-
-    for (int i = 1; i <= N; i++) {
-
-      // Get the value and weight of the item
-      int w = W[i - 1], v = V[i - 1];
-
-      for (int sz = 1; sz <= capacity; sz++) {
-
-        // Consider not picking this element
-        DP[i][sz] = DP[i - 1][sz];
-
-        // Consider including the current element and
-        // see if this would be more profitable
-        if (sz >= w && DP[i - 1][sz - w] + v > DP[i][sz]) DP[i][sz] = DP[i - 1][sz - w] + v;
-      }
-    }
-
+    int [][] DP = tableCreation(capacity, W, V, N);
     int sz = capacity;
     List<Integer> itemsSelected = new ArrayList<>();
 
@@ -70,6 +53,25 @@ public class Knapsack_01 {
 
     // Return the maximum profit
     return DP[N][capacity];
+  }
+  public static int[][] tableCreation(int capacity, int[] W, int[] V, int N){
+    int[][] DP = new int[N + 1][capacity + 1];
+    for (int i = 1; i <= N; i++) {
+
+      // Get the value and weight of the item
+      int w = W[i - 1], v = V[i - 1];
+
+      for (int sz = 1; sz <= capacity; sz++) {
+
+        // Consider not picking this element
+        DP[i][sz] = DP[i - 1][sz];
+
+        // Consider including the current element and
+        // see if this would be more profitable
+        if (sz >= w && DP[i - 1][sz - w] + v > DP[i][sz]) DP[i][sz] = DP[i - 1][sz - w] + v;
+      }
+    }
+    return DP;
   }
 
   public static void main(String[] args) {
