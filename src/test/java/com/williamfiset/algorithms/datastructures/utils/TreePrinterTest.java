6387f6ade5c051319f9514ad1cc1abf2cfb7f663
theanot <theanot@kth.se>
2022-02-23 10:14:24 +0100

    fix: calls the getTreeDisplay function

--- src/test/java/com/williamfiset/algorithms/datastructures/utils/TreePrinterTest.java
+++ src/test/java/com/williamfiset/algorithms/datastructures/utils/TreePrinterTest.java
@@ -3,6 +3,8 @@ package com.williamfiset.algorithms.datastructures.utils;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import com.williamfiset.algorithms.datastructures.balancedtree.AVLTreeRecursive;
+
 import org.junit.*;
 
 public class TreePrinterTest {
@@ -11,26 +13,25 @@ public class TreePrinterTest {
     private final PrintStream originalOut = System.out;
     private final PrintStream originalErr = System.err;
     private boolean[] test;
+    private AVLTreeRecursive<Integer> tree;
     
 
     @Before
         public void setup() {
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
+
+        tree = new AVLTreeRecursive<>();
     }
     
     @Test
     public void test() {
         test =new boolean [13];
-        printDebugInfo();
-    }
-
-    private void printDebugInfo() {
+        test = TreePrinter.getTreeDisplay(tree.root, test);
         for(int i = 0; i < test.length; i++) {
-          System.out.println(" " + test[i]);
-        }
-    
-      }
+            System.out.println(i + " : " + test[i]);
+          }
+    }
 
     @After
     public void restoreStreams() {
