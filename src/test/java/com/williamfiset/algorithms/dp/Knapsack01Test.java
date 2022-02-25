cae5b91e5d505ddd47087c687a8c8fe99a2c6501
OscarIngels <oscar.ingels@gmail.com>
2022-02-25 15:03:55 +0100

    fix: added a test to print out the coverage of the tests. #25

--- src/test/java/com/williamfiset/algorithms/dp/Knapsack01Test.java
+++ src/test/java/com/williamfiset/algorithms/dp/Knapsack01Test.java
@@ -7,12 +7,13 @@ import java.util.*;
 import org.junit.rules.ExpectedException;
 import org.junit.*;
 public class Knapsack01Test {
+    public Knapsack_01 obj = new Knapsack_01();
     @Test(expected = IllegalArgumentException.class)
     public void knapsack01InvalidInputTest1() {
         int capacity = 10;
         int[] V = {1, 4, 8};
         int[] W = {3, 3, 5, 6};
-        int val = Knapsack_01.knapsack(capacity, W, V);
+        int val = obj.knapsack(capacity, W, V);
 
     }
     /*Input:V = {1, 4, 8, 3}, W = {3, 3, 5, 6}, capacity = -1
@@ -24,21 +25,26 @@ public class Knapsack01Test {
         int capacity = -1;
         int[] V = {1, 4, 8, 3};
         int[] W = {3, 3, 5, 6};
-        int val = Knapsack_01.knapsack(capacity, W, V);
+        int val = obj.knapsack(capacity, W, V);
 
     }
+
     /*Input: V = null, W = {3, 3, 5, 6}, capacity = 10
      Valid weight and capacity, but null value array.
-     * Output: Throw IllegalArgumentException
+     Output: Throw IllegalArgumentException
      */
+
     @Test(expected = IllegalArgumentException.class)
     public void knapsack01InvalidInputTest3() {
         int capacity = 10;
         int[] V = null;
         int[] W = {3, 3, 5, 6};
-        int val = Knapsack_01.knapsack(capacity, W, V);
+        int val = obj.knapsack(capacity, W, V);
+        //cov3 = Knapsack_01.branch_coverage;
 
     }
+
+
     /*Input: V = {1, 4, 8, 3}, W = {3, 3, 5, 6}, capacity = 10
     Output: 12.
     */
@@ -47,9 +53,57 @@ public class Knapsack01Test {
         int capacity = 10;
         int[] V = {1, 4, 8, 3};
         int[] W = {3, 3, 5, 6};
-        int val = Knapsack_01.knapsack(capacity, W, V);
+        int val = obj.knapsack(capacity, W, V);
         assertThat(val).isEqualTo(12);
 
     }
+
+    @Test
+    public void runBranchCoverage(){
+        boolean[] coverage = new boolean[10];
+        //from correct output test
+        int capacity = 10;
+        int[] V = {1, 4, 8, 3};
+        int[] W = {3, 3, 5, 6};
+        int val = obj.knapsack(capacity, W, V);
+
+        for(int i = 0; i < coverage.length; i++){
+            if(obj.branch_coverage[i]) coverage[i] = true;
+        }
+
+        //From invalid input test 1
+        int[] V_2 = {1, 4, 8};
+        try {
+            val = obj.knapsack(capacity, W, V_2);
+        }catch(IllegalArgumentException e){}
+        for(int i = 0; i < coverage.length; i++){
+            if(obj.branch_coverage[i]) coverage[i] = true;
+        }
+        //from invalid input test 2
+        capacity = -1;
+        try {
+            val = obj.knapsack(capacity, W, V);
+        }catch(IllegalArgumentException e){}
+        for(int i = 0; i < coverage.length; i++){
+            if(obj.branch_coverage[i]) coverage[i] = true;
+        }
+        capacity = 10;
+        V = null;
+        try {
+            val = obj.knapsack(capacity, W, V);
+        }catch(IllegalArgumentException e){}
+        for(int i = 0; i < coverage.length; i++){
+            if(obj.branch_coverage[i]) coverage[i] = true;
+        }
+
+        for(int i = 0; i < coverage.length; i++){
+            System.out.println(coverage[i]);
+        }
+
+
+    }
+
+
+
 
 }
