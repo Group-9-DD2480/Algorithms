7c3d5ad18d683a1d5ac2e1ee850dc62069e3d42b
OscarIngels <oscar.ingels@gmail.com>
2022-02-25 10:32:39 +0100

    docs: Added contracts to unittests for knapsack. Relating to issue #7

--- src/test/java/com/williamfiset/algorithms/dp/Knapsack01Test.java
+++ src/test/java/com/williamfiset/algorithms/dp/Knapsack01Test.java
@@ -15,7 +15,10 @@ public class Knapsack01Test {
         int val = Knapsack_01.knapsack(capacity, W, V);
 
     }
-
+    /*Input:V = {1, 4, 8, 3}, W = {3, 3, 5, 6}, capacity = -1
+     Valid weight and value array, but negative capacity.
+    * Output: Throw IllegalArgumentException
+    */
     @Test(expected = IllegalArgumentException.class)
     public void knapsack01InvalidInputTest2() {
         int capacity = -1;
@@ -24,7 +27,10 @@ public class Knapsack01Test {
         int val = Knapsack_01.knapsack(capacity, W, V);
 
     }
-
+    /*Input: V = null, W = {3, 3, 5, 6}, capacity = 10
+     Valid weight and capacity, but null value array.
+     * Output: Throw IllegalArgumentException
+     */
     @Test(expected = IllegalArgumentException.class)
     public void knapsack01InvalidInputTest3() {
         int capacity = 10;
@@ -33,6 +39,9 @@ public class Knapsack01Test {
         int val = Knapsack_01.knapsack(capacity, W, V);
 
     }
+    /*Input: V = {1, 4, 8, 3}, W = {3, 3, 5, 6}, capacity = 10
+    Output: 12.
+    */
     @Test
     public void knapsack01CorrectOutputTest() {
         int capacity = 10;
