7813327c7e775c81b4118e9fe1cbd55f0f13c854
EdvardA1 <edvard.aldor@gmail.com>
2022-02-25 11:26:02 +0100

    fix: Added comments to unit tests issue #4

--- src/test/java/com/williamfiset/algorithms/math/ModPowTest.java
+++ src/test/java/com/williamfiset/algorithms/math/ModPowTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ModPowTest {
 
+    //Input 3,4,1000000, expected value is 81.
     @Test
     public void modPow() {
         ModPow obj = new ModPow();
@@ -25,6 +26,7 @@ public class ModPowTest {
         assertThat(r1.longValue()).isEqualTo(r2);
 
     }
+    //Expected Value:1,-2,39. Checks branch when n<0. Expected output:modPow(obj.modInv(a, m), -n, m)
     @Test
     public void modPowInvTest(){
         long a, n, m, r2;
@@ -35,7 +37,7 @@ public class ModPowTest {
         r2 = obj.modPow(a, n, m);
         assertThat(r2).isEqualTo(obj.modPow(obj.modInv(a, m), -n, m));
     }
-
+    // expected input: m=0, a,n doesn't matter. Expected output:ArithmeticException
     @Test(expected = ArithmeticException.class)
     public void modPowException1(){
         long a, n, m, r2;
@@ -46,6 +48,7 @@ public class ModPowTest {
         r2 = obj.modPow(a, n, m);
     }
 
+    // expected input: a=3,n=-2, m=39,(gcd(a,m) != 1). Expected output:ArithmeticException
     @Test(expected = ArithmeticException.class)
     public void modPowException2(){
         long a, n, m, r2;
