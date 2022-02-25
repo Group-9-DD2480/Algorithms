daf4593f99d8c30be21d8bcfafa0b1b48f8634a2
theanot <theanot@kth.se>
2022-02-25 13:55:59 +0100

    fix: coverage is printed for getTreeDisplay #35

--- src/test/java/com/williamfiset/algorithms/datastructures/utils/TreePrinterTest.java
+++ src/test/java/com/williamfiset/algorithms/datastructures/utils/TreePrinterTest.java
@@ -16,10 +16,13 @@ import org.junit.*;
 
 public class TreePrinterTest {
     private AVLTreeRecursive<Integer> tree;
+    TreePrinter obj = new TreePrinter();
+    boolean coverage[] =new boolean[22];
 
     @Before
         public void setup() {
         tree = new AVLTreeRecursive<>();
+        
     }
     
     //Input a Tree with only one node and
@@ -27,7 +30,8 @@ public class TreePrinterTest {
     @Test
     public void simpleGraph() {
         tree.insert(1);
-        String output = TreePrinter.getTreeDisplay(tree.root);
+        String output = obj.getTreeDisplay(tree.root);
+        
         Assert.assertEquals("1", output.toString().strip());
     }
 
@@ -41,8 +45,18 @@ public class TreePrinterTest {
         tree.insert(1);
         tree.insert(2);
         tree.insert(3);
-        String output = TreePrinter.getTreeDisplay(tree.root);
+        String output = obj.getTreeDisplay(tree.root);
         Assert.assertEquals("2     "+ "\n" + "   #######  " + "\n" + "   1     3", output.toString().strip());
+    }
+
+    @Test
+    public void allTest(){
+        simpleGraph();
+        TreeWithBraanches();
+        coverage = obj.coverage;
+        for (int i = 0; i < 22; i++){
+            System.out.println(i +" : " + coverage[i]);
+        }
     }
 
 }
